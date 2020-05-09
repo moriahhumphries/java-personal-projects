d5e0b8ea3a3ae8e7a37379bbc455e4c820e4d918
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-08 21:56:36 -0500

    completed primitive math types challenge

---
+++ src/PracticeMay8.java
@@ -0,0 +1,10 @@
+public class PracticeMay8 {
+    public static void main(String[] args){
+        byte myByte = 125;
+        short myShort = 2000;
+        int myInt = 60000;
+        long myLongTotal = 50000 + myByte + myShort + myInt;
+        System.out.println(myLongTotal);
+
+    }
+}
