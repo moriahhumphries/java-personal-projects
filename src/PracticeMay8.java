e0a2937a2d57e93febab85abb4d2bf7fbd8754ea
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-08 22:13:43 -0500

    completed lbs to kg calc

--- src/PracticeMay8.java
+++ src/PracticeMay8.java
@@ -3,8 +3,13 @@ public class PracticeMay8 {
         byte myByte = 125;
         short myShort = 2000;
         int myInt = 60000;
-        long myLongTotal = 50000 + myByte + myShort + myInt;
+        long myLongTotal = 50000L + 10 * (myByte + myShort + myInt);
         System.out.println(myLongTotal);
+
+        // calculate lbs to kg
+        double pounds = 17;
+        double lbsToKg = pounds * 0.45359237;
+        System.out.println(lbsToKg);
 
     }
 }
