e99c43820f2ce998d17859106c2282775ee5ce11
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-14 22:51:48 -0500

    completed wall area exercise

---
+++ src/WallAreaTest.java
@@ -0,0 +1,12 @@
+public class WallAreaTest {
+
+    public static void main(String[] args) {
+        WallArea wall = new WallArea(5,4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+
+    }
+}
