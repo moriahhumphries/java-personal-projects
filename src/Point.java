ced1bae3f2f8a2cdec63b5a0c5ae89d6ee6c7c79
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-16 17:20:20 -0500

    completed point challenge

--- src/Point.java
+++ src/Point.java
@@ -2,6 +2,14 @@ public class Point {
     private int x;
     private int y;
 
+    public Point() {
+        this(0,0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
     public int getX(){
         return x;
     }
@@ -18,9 +26,17 @@ public class Point {
         this.y = y;
     }
 
-    public double distance(int x, int y){
-        return x - y;
+    public double distance(){
+        return distance(0,0);
+    }
+    public double distance(int x, int y) {
+        return Math.sqrt((x-this.x)*(x-this.x)+(y-this.y)*(y-this.y));
     }
 
+    public double distance(Point point) {
+        int a = point.getX();
+        int b = point.getY();
+        return distance(a,b);
+    }
 
 }
