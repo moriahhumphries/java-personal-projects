ae0c147127e28fc0e39333fcc7217ac9d4505e0a
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-18 23:14:32 -0500

    completed fizzbuzz practice

---
+++ src/FizzBuzz.java
@@ -0,0 +1,16 @@
+class FizzBuzz {
+
+    public static void main(String[] args){
+        for(int i = 1; i <= 100; i++){
+            if (i % 15 == 0){
+                System.out.println("FizzBuzz: " + i);
+            } else if (i % 3 == 0){
+                System.out.println("Fizz: " + i);
+            } else if (i % 5 == 0){
+                System.out.println("Buzz: " + i);
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
