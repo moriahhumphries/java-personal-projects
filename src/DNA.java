bb3c8634f0f430a3656c3dd68d10a05a5e6cdc20
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-19 22:06:47 -0500

    completed dna project

---
+++ src/DNA.java
@@ -0,0 +1,27 @@
+public class DNA {
+
+    public static void main(String[] args) {
+        String dna1 = "ATGCGATACGCTTGA";
+        String dna2 = "ATGCGATACGTGA";
+        String dna3 ="ATTAATATGTACTGA";
+
+        String dna = dna3;
+        int length = dna.length();
+        System.out.println("Length: " + length);
+
+        int start = dna.indexOf("ATG");
+        System.out.println("Start: " + start);
+
+        int stop = dna.indexOf("TGA");
+        System.out.println("Stop: "+ stop);
+
+        if (start != -1 && stop != -1 && (stop - start) % 3 ==0) {
+            System.out.println("Condition 1 and 2 and 3 are satisfied.");
+            String protein = dna.substring(start, stop + 3);
+        } else {
+            System.out.println("No Protein");
+        }
+
+    }
+
+}
